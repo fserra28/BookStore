2340efc571b129588f9efa6fcd06902c42ef3557
Filipe Serra <fserra28@gmail.com>
2018-10-23 00:14:50 -0300

    Iniciado o projeto, criando dominio, conectando bd e habilitando migrations

---
+++ BookStore.Domain/Livro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Livro
+    {
+        public Livro()
+        {
+            this.Autores = new List<Autor>();
+
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome Requirido")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Campo Inválido!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Nome Requirido")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Campo Inválido!")]
+        public string ISBN { get; set; }
+
+        [Required(ErrorMessage = "Nome Requirido")]
+        [DataType(DataType.Date)] //Informa apenas a data, sem a necessidade de formatação.
+        public DateTime DataLancamento{ get; set; }
+
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+        public ICollection<Autor> Autores { get; set; }
+    }
+}
